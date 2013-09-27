bd7ce4d99cc274dd13cf774a3155bb68fe1690d5
Yulia <yulia.termanvelyants@gmail.com>
2013-09-27 19:49:31 +0400

    first commit

---
+++ src/main/java/PageGenerator.java
@@ -0,0 +1,33 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: yulia
+ * Date: 21.09.13
+ * Time: 10:53
+ * To change this template use File | Settings | File Templates.
+ */
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.Map;
+
+public class PageGenerator {
+    private static final String HTML_DIR = "tml";
+    private static final Configuration CFG = new Configuration();
+
+    public static String getPage(String filename, Map<String, Object> data) {
+        Writer stream = new StringWriter();
+        try {
+            Template template = CFG.getTemplate(HTML_DIR + File.separator + filename);
+            template.process(data, stream);
+        } catch (IOException | TemplateException e) {
+            e.printStackTrace();
+        }
+        return stream.toString();
+    }
+}
